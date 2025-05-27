d099d3332f9295d1fd0953f5c831bd8d0a0aa21e
Fraol_Lemma <fraollemma0506@gmail.com>
2025-05-28 01:56:38 +0300

    11settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-development-secret-key' 
 DEBUG = True  
-ALLOWED_HOSTS = ['poultry.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localpoultry.onrender.com', 'localhost', '127.0.0.1']
 
 LANGUAGES = [
     ('en', _('English')),
