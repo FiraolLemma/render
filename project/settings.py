609f8124f543c3f7a0481ebdbd988f32e54ee128
Fraol_Lemma <fraollemma0506@gmail.com>
2025-05-28 01:16:23 +0300

    33 settings.py

--- project/settings.py
+++ project/settings.py
@@ -92,7 +92,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]  
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
