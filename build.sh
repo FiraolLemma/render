0a48b5f7dc866abf6c9dcd4f66b2428b7f6d67cf
fraol lemma <fraollemma0506@gmail.com>
2025-06-27 22:59:37 +0300

    first commit3525525211

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations users
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py collectstatic --noinput
